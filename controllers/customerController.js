e11e90a23ca748f48614b1321c002e8ea612681d
alizeeshan604 <82883473+alizeeshan604@users.noreply.github.com>
2023-06-30 12:49:01 +0500

    Implemented Task

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -1,6 +1,22 @@
 const customerModel = require('../models/customerModel.js');
+const jwt = require('jsonwebtoken')
+const accessToken = require('../controllers/accesstoken.js')
+SECRET_ACCESS_TOKEN = "ijdhndkjbqiwhpoej1r3jh8as8fn129"
+SECRET_REFRESH_TOKEN = "ae23no2e9gf1n0241fklkawfonastt2"
 
+
+const authenticateToken = (req,res,next)=>{
+    if(accessToken == null){
+        return res.sendStatus(401);
+    }
+    jwt.verify(accessToken,SECRET_ACCESS_TOKEN,(err,data)=>{
+        if(err) return res.status(402).send(err);
+        req.user = data;
+        
+    })
+}
 const getCustomers = async (req, res) => {
+    await authenticateToken(req,res)
     const customers = await customerModel.listAllCustomers();
     res.status(200).send(customers);
 }
--- routes/orderRouter.js
+++ routes/orderRouter.js
@@ -2,6 +2,14 @@ const express = require('express');
 const router = express.Router();
 const orderController = require('../controllers/orderController.js');
 
+
+
+
 router.get('/', orderController.getOrders);
+router.get('/:id',orderController.getOrderById);
+router.post('/',orderController.newOrder);
+router.delete('/d',orderController.delOrder);
+
+
 
 module.exports = router;
